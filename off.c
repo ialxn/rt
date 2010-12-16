0de37e5ebff721d42f53aaa44dd7fad0e6c71374
Ivo Alxneit <ivo.alxneit@psi.ch>
2010-12-16 16:27:33 +0100

    framework to output geometry as 'OFF' files
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

---
+++ off.c
@@ -0,0 +1,12 @@
+/*	off.c
+ *
+ * Copyright (C) 2010 Ivo Alxneit
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#include "off.h"
+
---
+++ off.h
@@ -0,0 +1,14 @@
+/*	off.h
+ *
+ * Copyright (C) 2010 Ivo Alxneit
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+#ifndef __OFF_H__
+#define __OFF_H__
+
+#endif				/* __OFF_H__ */
