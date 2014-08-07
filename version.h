41990e9d32fd570cee857ae541889b22a9216e0b
Ivo Alxneit <ivo.alxneit@psi.ch>
2014-08-07 09:17:06 +0200

    tag new version
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

--- version.h
+++ version.h
@@ -11,7 +11,7 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define RELEASE "0.6.5"
-#define RELEASE_DATE "26.06.2014"
+#define RELEASE "0.6.6"
+#define RELEASE_DATE "07.08.2014"
 
 #endif				/* __VERSION_H__ */
