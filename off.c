f34ce3d4246ace2cfd06706a64171bb87bf3796c
Ivo Alxneit <alxneit@rutil>
2014-11-19 20:30:48 +0100

    write OFF files for target paraboloid
    Signed-off-by: Ivo Alxneit <alxneit@rutil>

--- off.c
+++ off.c
@@ -496,6 +496,128 @@ static void off_ellipsoid(const char *name, const double *origin,
 
 }
 
+static void off_paraboloid(const char *name, const double *origin,
+			   const double *Z, const double a,
+			   const double z_min, const double z_max,
+			   const double ri, const double gi,
+			   const double bi, const double ro,
+			   const double go, const double bo,
+			   const double dz)
+{
+#define N_TRANS 11
+#define N_ROT 24
+
+    int i, j;
+    double alpha, beta;
+    double O[] = { 0.0, 0.0, 0.0 };
+    const double delta_z = (z_max - z_min) / (N_TRANS - 1);
+    const double delta_phi = 2.0 * M_PI / N_ROT;
+    double l[3], g[3];
+    FILE *outf = open_off(name);
+
+    fprintf(outf, "OFF\n");
+    fprintf(outf, "%d %d 0\n", 2 * N_TRANS * N_ROT,
+	    2 * (N_TRANS - 1) * N_ROT);
+
+    /*
+     * determine alpha, beta for transformation from local to global system.
+     * discard 'l'
+     */
+    g2l_off(O, Z, l, &alpha, &beta);
+
+    /*
+     * calculate and output vertices
+     * outside surface ('ro', 'go', 'bo')
+     */
+    for (i = 0; i < N_TRANS; i++) {
+	double r;
+
+	l[2] = z_min + i * delta_z;
+	r = sqrt(4.0 * a * l[2]);
+
+	if (r < GSL_SQRT_DBL_EPSILON)
+	    r = GSL_SQRT_DBL_EPSILON;
+
+	for (j = 0; j < N_ROT; j++) {
+	    const double phi = j * delta_phi;
+
+	    l[0] = r * sin(phi);
+	    l[1] = r * cos(phi);
+
+	    l2g_off(origin, l, g, alpha, beta);
+
+	    fprintf(outf, "%f\t%f\t%f\n", g[0], g[1], g[2]);
+	}
+    }
+
+    /*
+     * calculate and output vertices
+     * inside surface scaled by 'dz' ('ri', 'gi', 'bi')
+     */
+    for (i = 0; i < N_TRANS; i++) {
+	double r;
+
+	l[2] = z_min + i * delta_z;
+	r = dz * sqrt(4.0 * a * l[2]);
+
+	if (r < GSL_SQRT_DBL_EPSILON)
+	    r = GSL_SQRT_DBL_EPSILON;
+
+	for (j = 0; j < N_ROT; j++) {
+	    const double phi = j * delta_phi;
+
+	    l[0] = r * sin(phi);
+	    l[1] = r * cos(phi);
+
+	    l2g_off(origin, l, g, alpha, beta);
+
+	    fprintf(outf, "%f\t%f\t%f\n", g[0], g[1], g[2]);
+	}
+    }
+
+    /*
+     * outside surface
+     * output faces (N_ROT=12, N_TRANS=5)
+     *   0       1      13      12       0
+     *   1       2      14      13       1
+     *           .
+     *  10      11      23      22      10
+     *  11       0      12      23      11
+     *  12      13      25      24      12
+     *           .
+     *           .
+     *           .
+     *  46      47      59      58      46
+     *  47      36      48      59      47
+     */
+    for (i = 0; i < (N_TRANS - 1) * N_ROT; i++)
+	if ((i + 1) % N_ROT)
+	    fprintf(outf, "5 %d\t%d\t%d\t%d\t%d\t%f\t%f\t%f 1.0\n",
+		    i, i + 1, i + 1 + N_ROT, i + N_ROT, i, ro, go, bo);
+	else
+	    fprintf(outf, "5 %d\t%d\t%d\t%d\t%d\t%f\t%f\t%f 1.0\n",
+		    i, i + 1 - N_ROT, i + 1, i + N_ROT, i, ro, go, bo);
+
+    /*
+     * same for inside surface
+     */
+    for (i = 0; i < (N_TRANS - 1) * N_ROT; i++)
+	if ((i + 1) % N_ROT)
+	    fprintf(outf, "5 %d\t%d\t%d\t%d\t%d\t%f\t%f\t%f 1.0\n",
+		    i + N_TRANS * N_ROT, i + 1 + N_TRANS * N_ROT,
+		    i + 1 + N_ROT + N_TRANS * N_ROT,
+		    i + N_ROT + N_TRANS * N_ROT, i + N_TRANS * N_ROT, ri,
+		    gi, bi);
+	else
+	    fprintf(outf, "5 %d\t%d\t%d\t%d\t%d\t%f\t%f\t%f 1.0\n",
+		    i + N_TRANS * N_ROT, i + N_TRANS * N_ROT + 1 - N_ROT,
+		    i + N_TRANS * N_ROT + 1, i + N_TRANS * N_ROT + N_ROT,
+		    i + N_TRANS * N_ROT, ri, gi, bi);
+
+    fclose(outf);
+
+}
+
 static void off_disk(const char *name, const double *origin,
 		     const double *dir, const double r, const double rf,
 		     const double gf, const double bf, const double rb,
@@ -1070,9 +1192,33 @@ static void output_targets(const config_t * cfg)
 	     *   tiny hole in the middle
 	     */
 	    off_window(name, O, Z, r, d, 0.39, 0.785, 0.785);
+	} else if (!strcmp(type, "paraboloid")) {
+	    double vertex[3], X[3], Y[3], Z[3];
+	    double foc;
+	    double z_min, z_max;
+	    const char *S;
+
+	    read_vector(this_t, "vertex", vertex);
+	    read_vector_normalize(this_t, "x", X);
+	    read_vector_normalize(this_t, "z", Z);
+	    orthonormalize(X, Y, Z);
+
+	    off_axes(name, vertex, X, Y, Z);	/*local system */
+
+	    config_setting_lookup_float(this_t, "focal_length", &foc);
+
+	    config_setting_lookup_float(this_t, "z_min", &z_min);
+	    config_setting_lookup_float(this_t, "z_max", &z_max);
+
+	    config_setting_lookup_string(this_t, "reflecting_surface", &S);
+	    if (!strcmp(S, "inside"))
+		off_paraboloid(name, vertex, Z, foc, z_min + DZ, z_max,
+			       1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0 - DZ);
+	    else
+		off_paraboloid(name, vertex, Z, foc, z_min, z_max, 0.0,
+			       0.0, 0.0, 1.0, 1.0, 1.0, 1.0 - DZ);
 
 	}
-
     }				/* end all targets */
 }
 
