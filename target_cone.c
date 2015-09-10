ialxn/rt

commit 2648ed6355348bd6b49c528ce1bd66e3632c5e8f

use function pointer for reflection function
do not use a generic 'reflect()' containing switch(model)

Signed-off-by: Ivo Alxneit <alxneit@rutil>

target_cone.c
@@ -26,7 +26,8 @@ typedef struct cone_state_t {
     double z_max;		/* height at radius 'r' */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -71,7 +72,8 @@ static int cone_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     state->flags |= init_reflecting_surface(this_target);
 
@@ -86,7 +88,8 @@ static void cone_free_state(void *vstate)
     cone_state_t *state = (cone_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 static double *cone_get_intercept(void *vstate, ray_t * ray)
@@ -171,7 +174,8 @@ static ray_t *cone_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	reflect(ray, N, hit, state->flags, r, state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_cylinder.c
@@ -25,7 +25,8 @@ typedef struct cyl_state_t {
     double l;			/* length of cylinder */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -65,7 +66,8 @@ static int cyl_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     state->flags |= init_reflecting_surface(this_target);
 
@@ -80,7 +82,8 @@ static void cyl_free_state(void *vstate)
     cyl_state_t *state = (cyl_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 static double *cyl_get_intercept(void *vstate, ray_t * ray)
@@ -165,7 +168,8 @@ static ray_t *cyl_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	reflect(ray, N, hit, state->flags, r, state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_ellipsoid.c
@@ -26,7 +26,8 @@ typedef struct ell_state_t {
     double z_min, z_max;	/* range of valid values of 'z' in local system */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -73,7 +74,8 @@ static int ell_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     state->flags |= init_reflecting_surface(this_target);
 
@@ -88,7 +90,8 @@ static void ell_free_state(void *vstate)
     ell_state_t *state = (ell_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 static double *ell_get_intercept(void *vstate, ray_t * ray)
@@ -168,7 +171,8 @@ static ray_t *ell_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	reflect(ray, N, hit, state->flags, r, state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_paraboloid.c
@@ -27,7 +27,8 @@ typedef struct par_state_t {
     double z_min, z_max;	/* range of valid values of 'z' in local system */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -73,7 +74,8 @@ static int par_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     state->flags |= init_reflecting_surface(this_target);
 
@@ -88,7 +90,8 @@ static void par_free_state(void *vstate)
     par_state_t *state = (par_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 static double *par_get_intercept(void *vstate, ray_t * ray)
@@ -169,7 +172,8 @@ static ray_t *par_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	reflect(ray, N, hit, state->flags, r, state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_sphere.c
@@ -23,7 +23,8 @@ typedef struct sph_state_t {
     double z_min, z_max;	/* range of valid values of 'z' in local system */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -67,7 +68,8 @@ static int sph_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     state->flags |= init_reflecting_surface(this_target);
 
@@ -82,7 +84,8 @@ static void sph_free_state(void *vstate)
     sph_state_t *state = (sph_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 
@@ -165,7 +168,8 @@ static ray_t *sph_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	reflect(ray, N, hit, state->flags, r, state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

reflect.c
@@ -21,8 +21,8 @@
 #include "reflect.h"
 
 
-static void reflect_specular(ray_t * r, const double N[3],
-			     const double P[3])
+void reflect_specular(ray_t * r, const double N[3], const double P[3],
+		      const gsl_rng * rng, void *model_params)
 /*
  * return specularly reflected ray 'r'. surface normal of reflecting surface
  * is 'N'. incoming ray has been determined before to intersect at 'P'. thus
@@ -33,10 +33,15 @@ static void reflect_specular(ray_t * r, const double N[3],
 
     cblas_daxpy(3, -2.0 * t, N, 1, r->dir, 1);	/* 'r' - 2 * 'N' dot 'r' * 'N' */
     memcpy(r->orig, P, 3 * sizeof(double));	/* update origin */
+
+    if (r->n_refl == UCHAR_MAX)
+	fprintf(stderr,
+		"                INFO: maximum path length exceeded (wrap around of counter occurs)\n");
+    ++r->n_refl;
 }
 
-static void reflect_lambertian(ray_t * r, const double N[3],
-			       const double P[3], const gsl_rng * rng)
+void reflect_lambertian(ray_t * r, const double N[3], const double P[3],
+			const gsl_rng * rng, void *model_params)
  /*
   * return diffusely reflected ray 'r'. surface normal of reflecting surface
   * is 'N'. incoming ray has been determined before to intersect at 'P'. thus
@@ -45,12 +50,16 @@ static void reflect_lambertian(ray_t * r, const double N[3],
 {
     get_uniform_random_vector_hemisphere(r->dir, 1.0, N, rng);
     memcpy(r->orig, P, 3 * sizeof(double));
+
+    if (r->n_refl == UCHAR_MAX)
+	fprintf(stderr,
+		"                INFO: maximum path length exceeded (wrap around of counter occurs)\n");
+    ++r->n_refl;
 }
 
-static void reflect_microfacet_gaussian(ray_t * r, const double N[3],
-					const double P[3],
-					const gsl_rng * rng,
-					const double sigma)
+void reflect_microfacet_gaussian(ray_t * r, const double N[3],
+				 const double P[3], const gsl_rng * rng,
+				 void *model_params)
 {
 /*
  * surface consists of uniformly distributed random facets. surface normal of
@@ -73,6 +82,7 @@ static void reflect_microfacet_gaussian(ray_t * r, const double N[3],
     double alpha, beta;
     double dummy[3];
     double original_ray_dir[3];
+    double *sigma = (double *) model_params;
 
     /*
      * determine alpha / beta to transform 'N' into local
@@ -92,7 +102,7 @@ static void reflect_microfacet_gaussian(ray_t * r, const double N[3],
 	memcpy(r->dir, original_ray_dir, 3 * sizeof(double));	/* restore */
 
 	do {			/* gaussian theta */
-	    theta = gsl_ran_gaussian(rng, sigma);
+	    theta = gsl_ran_gaussian(rng, *sigma);
 	} while (fabs(theta) > M_PI_2);
 
 	phi = 2.0 * M_PI * gsl_rng_uniform(rng);
@@ -109,40 +119,13 @@ static void reflect_microfacet_gaussian(ray_t * r, const double N[3],
 	 */
 	l2g_off_rot(random_N, new_N, alpha, beta);
 
-	reflect_specular(r, new_N, P);
+	reflect_specular(r, new_N, P, rng, model_params);
 	dot_product = cblas_ddot(3, r->dir, 1, N, 1);
 
     } while (dot_product < 0.0);	/* 'r' transmitted (not reflected) */
 
-}
-
-void reflect(ray_t * r, const double N[3], const double P[3],
-	     const int model, const gsl_rng * rng, void *model_params)
-{
-
-    switch (model & 0xFFFF) {
-
-    case SPECULAR:
-	reflect_specular(r, N, P);
-	break;
-
-    case LAMBERTIAN:
-	reflect_lambertian(r, N, P, rng);
-	break;
-
-    case MICROFACET_GAUSSIAN:
-	{
-	    double *sigma = (double *) model_params;
-
-	    reflect_microfacet_gaussian(r, N, P, rng, *sigma);
-	    break;
-	}
-
-    }
-
     if (r->n_refl == UCHAR_MAX)
 	fprintf(stderr,
 		"                INFO: maximum path length exceeded (wrap around of counter occurs)\n");
-
     ++r->n_refl;
 }

reflect.h
@@ -15,18 +15,18 @@
 
 #include "ray.h"
 
-/*
- * reflectivity models
- * use bits 1-15 of an int
- */
 #define MODEL_NONE		0
-#define SPECULAR		(1<<0)
-#define LAMBERTIAN		(1<<1)
-#define MICROFACET_GAUSSIAN	(1<<2)
 
 
-extern void reflect(ray_t * r, const double N[3], const double P[3],
-		    const int model, const gsl_rng * rng,
-		    void *model_params);
+extern void reflect_specular(ray_t * r, const double N[3],
+			     const double P[3], const gsl_rng * rng,
+			     void *model_params);
+extern void reflect_lambertian(ray_t * r, const double N[3],
+			       const double P[3], const gsl_rng * rng,
+			       void *model_params);
+extern void reflect_microfacet_gaussian(ray_t * r, const double N[3],
+					const double P[3],
+					const gsl_rng * rng,
+					void *model_params);
 
 #endif				/* __REFLECT_H__ */

target_annulus.c
@@ -25,7 +25,8 @@ typedef struct ann_state_t {
     double r2;			/* inner radius^2 of annulus */
     double *M;			/* transform matrix local -> global coordinates */
     gsl_spline *refl_spectrum;	/* for interpolated reflectivity spectrum */
-    void *refl_model_params;
+    refl_func_pointer_t refl_func;	/* reflection model */
+    void *refl_model_params;	/* model specific parameters */
     union fh_t output;		/* output file handle or name */
     int flags;
     pthread_key_t PTDT_key;	/* access to output buffer and flags for each target */
@@ -63,7 +64,8 @@ static int ann_init_state(void *vstate, config_setting_t * this_target,
 	state->flags |= MODEL_NONE;
 	return ERR;
     }
-    init_refl_model(this_target, &state->flags, &state->refl_model_params);
+    init_refl_model(this_target, &state->refl_func,
+		    &state->refl_model_params);
 
     config_setting_lookup_float(this_target, "R", &t);
     state->R2 = t * t;
@@ -81,7 +83,8 @@ static void ann_free_state(void *vstate)
     ann_state_t *state = (ann_state_t *) vstate;
 
     state_free(state->output, state->flags, state->M,
-	       state->refl_spectrum, state->refl_model_params);
+	       state->refl_spectrum, state->refl_func,
+	       state->refl_model_params);
 }
 
 static double *ann_get_intercept(void *vstate, ray_t * ray)
@@ -163,8 +166,8 @@ static ray_t *ann_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	return NULL;
 
     } else {			/* reflect 'ray' */
-	reflect(ray, &state->M[6], hit, state->flags, r,
-		state->refl_model_params);
+	state->refl_func(ray, &state->M[6], hit, r,
+			 state->refl_model_params);
 
 	data->flag |= LAST_WAS_HIT;	/* mark as hit */
 

commit 57f485f76d23f22f5afc1374d9c0393e3c69c9be

BUGFIX: copy / paste error (wrong parameter passed) introduced in 2648ed63
Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

target_cone.c
@@ -175,7 +175,7 @@ static ray_t *cone_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	state->refl_func(ray, &state->M[6], hit, r, state->refl_func_pars);
+	state->refl_func(ray, N, hit, r, state->refl_func_pars);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_cylinder.c
@@ -169,7 +169,7 @@ static ray_t *cyl_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	state->refl_func(ray, &state->M[6], hit, r, state->refl_func_pars);
+	state->refl_func(ray, N, hit, r, state->refl_func_pars);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_ellipsoid.c
@@ -172,7 +172,7 @@ static ray_t *ell_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	state->refl_func(ray, &state->M[6], hit, r, state->refl_func_pars);
+	state->refl_func(ray, N, hit, r, state->refl_func_pars);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_paraboloid.c
@@ -173,7 +173,7 @@ static ray_t *par_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	state->refl_func(ray, &state->M[6], hit, r, state->refl_func_pars);
+	state->refl_func(ray, N, hit, r, state->refl_func_pars);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */

target_sphere.c
@@ -169,7 +169,7 @@ static ray_t *sph_get_out_ray(void *vstate, ray_t * ray, double *hit,
 	if (!(state->flags & OUTSIDE))
 	    cblas_dscal(3, -1.0, N, 1);	/* make normal point inwards */
 
-	state->refl_func(ray, &state->M[6], hit, r, state->refl_func_pars);
+	state->refl_func(ray, N, hit, r, state->refl_func_pars);
 
 	if (data->flag & ICPT_ON_CONVEX_SIDE) {
 	    data->flag |= LAST_WAS_HIT;	/* mark as hit */