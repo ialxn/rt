65458880dcb633a826bf8ca5cd04fd7a075aca51
Ivo Alxneit <ivo.alxneit@psi.ch>
2016-01-18 11:27:54 +0100

    update copyright
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

--- likely.h
+++ likely.h
@@ -1,6 +1,6 @@
 /*	likely.h
  *
- * Copyright (C) 2015 Ivo Alxneit
+ * Copyright (C) 2015,2016 Ivo Alxneit
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
