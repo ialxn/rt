e4064f3013b2e31fbaee8539e43faa24276c03bf
Ivo Alxneit <ivo.alxneit@psi.ch>
2018-01-31 12:46:11 +0100

    update copyright info and add GPL3 specifics
    Signed-off-by: Ivo Alxneit <ivo.alxneit@psi.ch>

--- source_solid_cone.c
+++ source_solid_cone.c
@@ -1,10 +1,21 @@
 /*	target_cone.c
  *
- * Copyright (C) 2014,2015,2016,2017 Ivo Alxneit
+ * Copyright (C) 2014 - 2018 Ivo Alxneit, Paul Scherrer Institute
  *
- * This program is distributed in the hope that it will be useful, but
+ * This file is part of rt
+ *
+ * rt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rt is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rt. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define _ISOC99_SOURCE		/* because of llrint() */
